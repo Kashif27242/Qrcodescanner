4c65006a3dd4198375965ec373cdc3a498310378
Kashiif27242 <kashifturk014@gmail.com>
2023-06-19 22:48:40 +0500

    okay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import jsQR from 'jsqr';
 
-function App() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [QRCodeContent, setQRCodeContent] = useState('');
+
+  const handleScan = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+    setQRCodeContent('');
+    decodeQRCode(file);
+  };
+
+  const decodeQRCode = (file) => {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    img.onload = function () {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const code = jsQR(imageData.data, canvas.width, canvas.height);
+      if (code) {
+        setQRCodeContent(code.data);
+      } else {
+        setQRCodeContent('');
+      }
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">QR Code Scanner</h1>
+      <input type="file" accept="image/*" onChange={handleScan} />
+      {selectedImage && <img className="qr-image" src={selectedImage} alt="Selected QR Code" />}
+      {QRCodeContent ? (
+        <div className="result">
+          <h2 className="result-title">QR Code Content:</h2>
+          <p className="result-content">{QRCodeContent}</p>
+        </div>
+      ) : (
+        <p className="no-result">No QR Code found or unable to decode.</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
